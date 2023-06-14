5fb8af6458ecd34365f9c719d7dd745e71ed2e40
enyllaoliveira <enyllaoliveira@gmail.com>
2023-06-14 11:20:08 -0300

    Tratativa das condicionais do IMC

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 let nome = document.getElementById('inf-name')
-let alt = document.getElementById('inf-alt')
-let peso = document.getElementById('inf-peso')
 let botao = document.getElementById('btn')
 let containerInf = document.getElementById("rodape")
 
 botao.addEventListener('click', clique)
 function clique() {
-    let peso1 = Number(peso.value)
-    let alt1 = Number(alt.value)
-    let imc = (((peso1 * 100) / (alt1 * alt1))  * 100 ).toFixed(2)
-    containerInf.innerHTML = `Olá, ${nome.value}. <br>
-     Seu IMC atual é de ${imc}`
-}
+    let peso = Number(document.getElementById('inf-peso').value)
+    let alt = Number(document.getElementById('inf-alt').value)
+    let imc = (((peso * 100) / (alt * alt))  * 100 ).toFixed(2)
+    let situacao = ''
+
+    if (imc  < 18.5) {
+      situacao = 'magreza'
+    } else if (imc > 18.5 && imc < 24.9) {
+      situacao = 'normal'
+    } else if ( imc > 25 && imc < 29.9 ) {
+    situacao = 'sobrepeso I'
+    } else if (imc > 30 && imc < 39.9) {
+    situacao = 'obesidade I'
+    } else {
+      situacao = 'grave III'
+    }
+
+    containerInf.innerHTML = `<p> Olá, ${nome.value}.</p> 
+     <p> Seu IMC atual é de ${imc}</p>
+     <p>  Sua situação é de ${situacao} </p>`
+    }
+
+
+
+ 
+
+  
