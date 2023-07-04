ca90c0e925496e270ea6c0babd87b65d8d759571
enyllaoliveira <enyllaoliveira@gmail.com>
2023-07-04 10:51:20 -0300

    habilitar/desabilitar botão

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 let nome = document.getElementById('inf-name')
-let peso1 = document.getElementById('inf-peso')
-let alt1 = document.getElementById('inf-alt')
+let peso = document.getElementById('inf-peso')
+let alt = document.getElementById('inf-alt')
 let containerInf = document.getElementById("rodape")
 const botao = document.getElementById('btn')
 
-botao.addEventListener('submit', checkInf)
-function checkInf() {
-  if (nome == '') {
-    botao.disabled = true;
-    nome.innerHTML = `Esse campo é obrigatório`
-  } else if(peso1 == '') {
-    botao.disabled = true;
-    peso1.inner = `Esse campo é obrigatório`
-  } else if (alt1  == '') {
-    botao.disabled = true;
-    alt1.innerHTML = `Esse campo é obrigatório`
-  } else {
-    botao.disabled = false;
-  }
+botao.classList.add('disabled')
+botao.style.pointerEvents = 'none'
+initial()
+
+function initial() {
+nome.addEventListener('keyup', (e) => {
+if (e.target.value != '') {
+  botao.classList.remove('disabled')
+  botao.removeAttribute('style')
+} else {
+  botao.classList.add('disabled')
+  botao.style.pointerEvents = 'none'
 }
+})
+
+peso.addEventListener('keyup', (e) => {
+  if (e.target.value != '') {
+    botao.classList.remove('disabled')
+    botao.removeAttribute('style')
+  } else {
+    botao.classList.add('disabled')
+    botao.style.pointerEvents = 'none'
+  }
+  })
+
+  alt.addEventListener('keyup', (e) => {
+    if (e.target.value != '') {
+      botao.classList.remove('disabled')
+      botao.removeAttribute('style')
+    } else {
+      botao.classList.add('disabled')
+      botao.style.pointerEvents = 'none'
+    }
+    })
+  }
 
 botao.addEventListener('click', calcular)
 function calcular() {
-    let peso = Number(document.getElementById('inf-peso').value)
-    let alt = Number(document.getElementById('inf-alt').value)
-    let imc = (((peso * 100) / ((alt * alt / 100)))).toFixed(2)
+  console.log(peso, alt)
+  let pesoConvertido = Number(peso.value)
+  let altConvertido = Number(alt.value)
+    let imc = (((pesoConvertido * 100) / ((altConvertido * altConvertido / 100)))).toFixed(2)
     let situacao = ''
 
     if (imc  < 18.5) {
@@ -43,28 +64,4 @@ function calcular() {
      <p>  Sua situação é de ${situacao} </p>`
     }
 
- 
- 
-    
 
-    // botao.addEventListener ('click', checkInf)
-    // function checkInf() {
-    //   let name = document.getElementById('inf-name').value;
-    //   let peso = Number(document.getElementById('inf-peso').value);
-    //   let alt = Number(document.getElementById('inf-alt').value);
-
-    //   if (name === '') {
-    //     document.getElementById('btn').disabled = true; 
-    //     document.getElementById('name').textContent = "Esse campo é obrigatório";
-    //   } else if(alt === '') {
-    //     document.getElementById('btn').disabled = true; 
-    //     document.getElementById('alt').textContent = "Esse campo é obrigatório";
-    //   } else if(peso === '' ) {
-    //     document.getElementById('btn').disabled = true; 
-    //     document.getElementById('peso').textContent = "Esse campo é obrigatório";
-    //   } else {
-    //     document.getElementById('btn').disabled = false; 
-    //   }
-    //   }
-
-          // document.getElementById('btn').disabled = true;
